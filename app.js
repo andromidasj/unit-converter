c884c3544643f3376fb144145c404e9d02131832
Josh Andromidas <git@joshandromidas.com>
2022-02-24 23:10:59 -0600

    added input fields and some logic to process unit type clicks

--- app.js
+++ app.js
@@ -1,4 +1,51 @@
 $(document).ready(function(){
-  var $app = $('#app');
-  $app.append("<div>test</div>")
+  // let $app = $('#app');
+  // let $topBar = $('<div id="top-bar"><span id="title">Unit Converter</span></div>');
+  // let $contentContainer = $('<div id="content-container"></div>');
+  // let $unitSelector = $('<ul id="unit-selector"></ul>');
+  let $unitSelector = $('#unit-selector');
+  // let $inOutContainer = $('<div id="in-out-container"></div>');
+  // let $inputContainer = $('<div id="input-container"></div>');
+  // let $outputContainer = $('<div id="output-container"></div>');
+
+  // $app.append($topBar);
+  // $app.append($contentContainer);
+  // $contentContainer.append($unitSelector)
+  // $contentContainer.append($inOutContainer)
+  // $inOutContainer.append($inputContainer)
+  // $inOutContainer.append($outputContainer);
+  console.log(JSON.stringify(allUnits));
+
+  const changeUnitType = function(e) {
+    $('.unit-type').removeClass('selected');
+    $(e.target).addClass('selected');
+    console.log(e.target.innerHTML);
+    renderLeftUnits(e.target.innerHTML);
+  }
+
+  const renderLeftUnits = function(unitType) {
+    unitType = unitType || 'length';
+    $('.left-selections-container li').remove();
+    for (let key in allUnits[unitType].subtypes) {
+      let $li = $('<li class="unit-subtypes"></li>');
+      $($li).html(key);
+      $('.left-selections-container').append($li);
+    }
+  }
+
+  const renderUnitSelector = function() {
+    let unitTypes = ['length', 'weight', 'volume', 'speed', 'time'];
+    unitTypes.forEach((e, i) => {
+      let $listItem = $('<li class="unit-type"></li>');
+      if (i === 0) {
+        $listItem.addClass('selected')
+      }
+      $listItem.html(e);
+      $listItem.click(changeUnitType);
+      $unitSelector.append($listItem);
+    });
+  }
+
+  renderLeftUnits();
+  renderUnitSelector();
 });
---
+++ units.js
@@ -0,0 +1,24 @@
+const allUnits = {
+  length: {
+    measure_unit: 'millimeters',
+    subtypes: {
+      mile: 1609344.01,
+      foot: 304.8,
+      inch: 25.4,
+      meter: 1000,
+      centimeter: 10,
+      millimeter: 1,
+    },
+  },
+  weight: {
+    measure_unit: 'milligram',
+    subtypes: {
+      pound: 1,
+      stone: 1,
+      ton: 1,
+      kilogram: 1,
+      gram: 1,
+      milligram: 1,
+    },
+  }
+}
