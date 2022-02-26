0f2d308a396d8c7d42ac731c5d1efe10b8df5ca7
Josh Andromidas <git@joshandromidas.com>
2022-02-25 23:16:18 -0600

    working conversion for lengths!

--- app.js
+++ app.js
@@ -1,40 +1,108 @@
 $(document).ready(function(){
-  // let $app = $('#app');
-  // let $topBar = $('<div id="top-bar"><span id="title">Unit Converter</span></div>');
-  // let $contentContainer = $('<div id="content-container"></div>');
-  // let $unitSelector = $('<ul id="unit-selector"></ul>');
   let $unitSelector = $('#unit-selector');
-  // let $inOutContainer = $('<div id="in-out-container"></div>');
-  // let $inputContainer = $('<div id="input-container"></div>');
-  // let $outputContainer = $('<div id="output-container"></div>');
 
-  // $app.append($topBar);
-  // $app.append($contentContainer);
-  // $contentContainer.append($unitSelector)
-  // $contentContainer.append($inOutContainer)
-  // $inOutContainer.append($inputContainer)
-  // $inOutContainer.append($outputContainer);
-  console.log(JSON.stringify(allUnits));
+  $('#input-field-left').on('input', function(e){
+    let thisVal = e.target.value;
+    let inputUnit = $('.unit-subtype-left.selected').html();
+    let outputUnit  = $('.unit-subtype-right.selected').html();
+    let otherVal = calculateConversion(thisVal, inputUnit, outputUnit);
+    $('#input-field-right').val(otherVal);
+  });
+
+  $('#input-field-right').on('input', function(e){
+    let thisVal = e.target.value;
+    let inputUnit = $('.unit-subtype-right.selected').html();
+    let outputUnit  = $('.unit-subtype-left.selected').html();
+    let otherVal = calculateConversion(thisVal, inputUnit, outputUnit);
+    $('#input-field-left').val(otherVal);
+  });
+
+  const calculateConversion = function(inputVal, inputUnit, outputUnit) {
+    let outputVal;
+    let unitType = $('.unit-type.selected').html();
+    console.log('calculating...');
+    console.log(inputVal, inputUnit, 'to', outputUnit);
+
+    // Formula for calculating conversion based on measure_unit;
+    outputVal = allUnits[unitType].subtype[inputUnit] * inputVal / allUnits[unitType].subtype[outputUnit];
+
+    // Round to 3 decimal places
+    return Math.round(outputVal * 1000) / 1000;
+  }
 
   const changeUnitType = function(e) {
     $('.unit-type').removeClass('selected');
     $(e.target).addClass('selected');
-    console.log(e.target.innerHTML);
-    renderLeftUnits(e.target.innerHTML);
+    renderUnits(e.target.innerHTML);
   }
 
-  const renderLeftUnits = function(unitType) {
+  const changeUnit = function(e) {
+    // TODO: maybe make a better way to find subTypeClass
+    // that can account for the classList being in a
+    // different order. Even needed? 🤔
+
+    // side1 and unit1 are what is being clicked, calculated and changed.
+    // side2 and unit2 are the other side that stays constant.
+    let side1 = e.target.classList[0].split('-')[2];
+    let side2 = side1 === 'right' ? 'left' : 'right';
+    let inputField1 = '#input-field-' + side1;
+    let inputField2 = '#input-field-' + side2;
+    let subtypeClass = '.' + e.target.classList[0];
+    // console.log(e.target.classList)
+    // console.log(subtypeClass)
+    // console.log(side)
+    $(subtypeClass).removeClass('selected');
+    $(e.target).addClass('selected');
+
+    // let inputUnit1 = '.unit-subtype-' + side1 + '.selected'
+    // let inputUnit2 = '.unit-subtype-' + side2 + '.selected';
+    // console.log('inputUnit1', inputUnit1)
+    // console.log('inputUnit2', inputUnit2)
+    let inputUnit = $('.unit-subtype-'+side2+'.selected').html();
+    let outputUnit = $('.unit-subtype-'+side1+'.selected').html();
+    console.log(inputUnit, outputUnit)
+    console.log(inputField1)
+    let outputVal;
+    // calculateConversion();
+    // console.log(calculateConversion($(inputField2).val(), inputUnit, outputUnit))
+    $(inputField1).val(calculateConversion($(inputField2).val(), inputUnit, outputUnit));
+  }
+
+  const renderUnits = function(unitType) {
+    // Here we render the units on both the left
+    // and right unit list containers under the
+    // number input field
     unitType = unitType || 'length';
     $('.left-selections-container li').remove();
-    for (let key in allUnits[unitType].subtypes) {
-      let $li = $('<li class="unit-subtypes"></li>');
-      $($li).html(key);
-      $('.left-selections-container').append($li);
+    $('.right-selections-container li').remove();
+
+    let counter = 0;
+    for (let key in allUnits[unitType].subtype) {
+      let $liLeft = $('<li class="unit-subtype-left"></li>');
+      let $liRight = $('<li class="unit-subtype-right"></li>');
+      $liLeft.html(key);
+      $liRight.html(key);
+      // Set default selected units
+      if (counter === 0) {
+        $liLeft.addClass('selected');
+      }
+      if (counter === 1) {
+        $liRight.addClass('selected');
+      }
+      $liLeft.click(changeUnit);
+      $liRight.click(changeUnit);
+      $('.right-selections-container').append($liRight);
+      $('.left-selections-container').append($liLeft);
+      counter++;
     }
   }
 
-  const renderUnitSelector = function() {
-    let unitTypes = ['length', 'weight', 'volume', 'speed', 'time'];
+  const renderUnitTypes = function() {
+    let unitTypes = [];
+    for (let key in allUnits) {
+      unitTypes.push(key);
+    }
+    unitTypes.sort();
     unitTypes.forEach((e, i) => {
       let $listItem = $('<li class="unit-type"></li>');
       if (i === 0) {
@@ -46,6 +114,24 @@ $(document).ready(function(){
     });
   }
 
-  renderLeftUnits();
-  renderUnitSelector();
+  renderUnits();
+  renderUnitTypes();
 });
+
+
+  // jQuery method of creating elements, easier to just do in HTML
+  // let $app = $('#app');
+  // let $topBar = $('<div id="top-bar"><span id="title">Unit Converter</span></div>');
+  // let $contentContainer = $('<div id="content-container"></div>');
+  // let $unitSelector = $('<ul id="unit-selector"></ul>');
+  // let $inOutContainer = $('<div id="in-out-container"></div>');
+  // let $inputContainer = $('<div id="input-container"></div>');
+  // let $outputContainer = $('<div id="output-container"></div>');
+
+
+  // $app.append($topBar);
+  // $app.append($contentContainer);
+  // $contentContainer.append($unitSelector)
+  // $contentContainer.append($inOutContainer)
+  // $inOutContainer.append($inputContainer)
+  // $inOutContainer.append($outputContainer);
--- units.js
+++ units.js
@@ -1,7 +1,7 @@
 const allUnits = {
   length: {
-    measure_unit: 'millimeters',
-    subtypes: {
+    measure_unit: 'millimeter',
+    subtype: {
       mile: 1609344.01,
       foot: 304.8,
       inch: 25.4,
@@ -12,13 +12,32 @@ const allUnits = {
   },
   weight: {
     measure_unit: 'milligram',
-    subtypes: {
+    subtype: {
       pound: 1,
       stone: 1,
       ton: 1,
       kilogram: 1,
       gram: 1,
       milligram: 1,
+    },
+  },
+  time: {
+    measure_unit: 'second',
+    subtype: {
+      second: 1,
+      minute: 1,
+      hour: 1,
+      day: 1,
+      week: 1,
+      year: 1,
+    },
+  },
+  temperature: {
+    measure_unit: 'kelvin',
+    subtype: {
+      celsius: 1,
+      kelvin: 1,
+      fahrenheit: 1,
     },
   }
 }
